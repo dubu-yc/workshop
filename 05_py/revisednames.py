57ab4b82ef40e9a11b5818824f576899ac0f8dc9
Yoonah Chang <77413683+dubu-yc@users.noreply.github.com>
2021-09-27 09:22:36 -0400

    Update revisednames.py
    in class work 9/27

--- 05_py/revisednames.py
+++ 05_py/revisednames.py
@@ -3,16 +3,22 @@
 #K05 -- REVISED Python Random Name Generator
 #2021-09-26
 
-#discovered that randint > rand, adding "period 1/2: " makes output more readable
+#SUMMARY OF TRIO DISCUSSION
+#good code is reusable code.
+#DISCOVERIES
+#randint > rand, adding "period 1/2: " makes output more readable
+#QUESTIONS
 #should the lists be a separate file? are mini lists better for testing to see if it works?
+#COMMENTS
 #contributions- yoonah: period 1/2 comments, josh: randint and lists, josephine: use of a function
 
 import random
-
-list1 = ["Rayat", "William", "Michelle", "Lucas", "Ivan"];
-list2 = ["Yoonah", "Joshua", "Alif", "Josephine", "Andrew"];
+students = {
+        'list1': ["Rayat", "William", "Michelle", "Lucas", "Ivan"],
+        'list2': ["Yoonah", "Joshua", "Alif", "Josephine", "Andrew"]
+}
 
 def printName():
-    print("period 1: " + list1[random.randint(0, len(list1) - 1)])
-    print("period 2: " + list2[random.randint(0, len(list2) - 1)])
+    print("period 1: " + students["list1"][random.randint(0, len(list1) - 1)])
+    print("period 2: " + students["list2"][random.randint(0, len(list2) - 1)])
 printName()
