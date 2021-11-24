47631e888750387daae83d184268ddac835cdf14
Yoonah Chang <77413683+dubu-yc@users.noreply.github.com>
2021-11-23 21:13:39 -0500

    Update app.py

--- 19_api/app.py
+++ 19_api/app.py
@@ -5,7 +5,7 @@
 # time spent: 1.0 hours
 
 from flask import Flask, render_template
-import request
+import requests
 app = Flask(__name__) 
 
 with open("key_nasa.txt", "r", encoding="utf-8") as api:
@@ -13,7 +13,7 @@ with open("key_nasa.txt", "r", encoding="utf-8") as api:
 
 @app.route("/") 
 def main():
-    api_request = request.get("https://api.nasa.gov/planetary/apod", params={"api_key": NasaAPIKey})
+    api_request = requests.get("https://api.nasa.gov/planetary/apod", params={"api_key": NasaAPIKey})
     if api_request.status_code == 200:
         pic_explanation = api_request.json()["explanation"]
         pic = api_request.json()["hdurl"]
