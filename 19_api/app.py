b4328e753f6ac8ab8e490c57c4bd87646be20dfa
Yoonah Chang <77413683+dubu-yc@users.noreply.github.com>
2021-11-23 21:12:54 -0500

    Update app.py

--- 19_api/app.py
+++ 19_api/app.py
@@ -13,7 +13,7 @@ with open("key_nasa.txt", "r", encoding="utf-8") as api:
 
 @app.route("/") 
 def main():
-    api_request = requests.get("https://api.nasa.gov/planetary/apod", params={"api_key": NasaAPIKey})
+    api_request = request.get("https://api.nasa.gov/planetary/apod", params={"api_key": NasaAPIKey})
     if api_request.status_code == 200:
         pic_explanation = api_request.json()["explanation"]
         pic = api_request.json()["hdurl"]
