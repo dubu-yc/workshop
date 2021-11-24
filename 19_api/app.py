59dd2817c553e0ea12d78a5732206d9571294235
Yoonah Chang <77413683+dubu-yc@users.noreply.github.com>
2021-11-23 21:26:12 -0500

    Update app.py

--- 19_api/app.py
+++ 19_api/app.py
@@ -26,7 +26,7 @@ def main():
     print (imglink)
     print (pic_explanation)
 
-    return render_template("main.html", img = imglink, pic_explanation)
+    return render_template("main.html", img = imglink, explanation = pic_explanation)
 
 if __name__ == "__main__":
     app.debug = True
