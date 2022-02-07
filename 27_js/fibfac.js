b19c370b1c7270942865bead21d6489563192ff7
dubu-yc <ichang20@stuy.edu>
2022-02-07 16:10:52 -0500

    fix merge

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
-=======
 // Team Zemmi :: Clyde Sinclair, Fierce Dragon
->>>>>>> 0054823575d5fbbd9b0fde12cde3a2069f9c7d41
 // SoftDev pd0
 // K27 -- Basic functions in JavaScript
 // 2022-02-03r
@@ -12,9 +8,6 @@
 // as a duo...
 // pair programming style,
 // implement a fact and fib fxn
-<<<<<<< HEAD
-
-=======
 var fact = function(n) {
 if(n < 1){
    return 1
@@ -28,6 +21,5 @@ var fib = function(n) {
   }
   return fib(n - 1) + fib(n - 2);
 };
->>>>>>> 0054823575d5fbbd9b0fde12cde3a2069f9c7d41
 
 //Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
