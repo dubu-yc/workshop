3a28d62edd3e8d0bf4e48b800b76a1b7ecda225d
dubu-yc <ichang20@stuy.edu>
2022-02-16 15:16:11 -0500

    fixed code from my work at dojo

--- 30_jsdraw/clickdraw.js
+++ 30_jsdraw/clickdraw.js
@@ -1,62 +1,61 @@
 //retrieve node in DOM via ID
-var c = document.getElementById("slate")
+var c = document.getElementById("slate");
 
 //instantiate a CanvasRenderingContext2D Object
 var ctx = c.getContext("2d");
 
 //intit global state var
 var mode = "rect";
+var toggler = document.getElementById("buttonToggle");
+
 
 //var toggleMode = function(e) {
-var toggleMode = function(e) {
-    console.log("toggling...")
+var toggleMode = (e) => {
     if (mode == "rect") {
-      mode = "circle";
+        mode = "circle";
+        toggler.innerHTML = "Circle"
+    } else {
+        mode = "rect";
+        toggler.innerHTML = "Rect"
     }
-    else {
-      mode = "rect";
-    }
 }
 
-var drawRect = function(e) {
-    var mouseX = e.offsetX;
-    var mouseY = e.offsetY;
-    ctx.beginPath()
-    ctx.rect(mouseX, mouseY, 100, 50)
-    ctx.fill();
-    ctx.stroke();
-    console.log("mouseclick registered at ", mouseX, mouseY);
-}
+toggler.addEventListener("click", toggleMode);
 
-//var drawCircle = function(e) {
-var drawCircle = (e) => {
-    var mouseX = e.offsetX;
-    var mouseY = e.offsetY;
-    ctx.beginPath()
-    ctx.arc(mouseX, mouseY, 50, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.stroke();
-    console.log("mouseclick registered at ", mouseX, mouseY);
-}
-
-//var draw = function(e) {
 var draw = (e) => {
-  if (mode == "rect") {
-    drawRect(e);
-  }
-  else {
-    drawCircle(e);
-  }
+    if (mode == "rect") {
+       drawRect(e);
+    } else {
+        drawCircle(e);
+    }
 }
 
-//var wipeCanvas = function() {
-var wipeCanvas = () => {
-    console.log("wiping?");
-    ctx.clearRect(0,0, c.width, c.height);
+var drawRect = (e) => {
+    let mouseX = e.offsetX;
+    let mouseY = e.offsetY;
+
+    ctx.rect(mouseX, mouseY, 100, 150)
+    ctx.fillStyle = "green"
+    ctx.fill()
 }
+
+var drawCircle = (e) => {
+    let mouseX = e.offsetX;
+    let mouseY = e.offsetY;
+    
+    ctx.beginPath()
+    ctx.arc(mouseX, mouseY, 50, 0, 2 * Math.PI)
+    ctx.fillStyle = "blue"
+    ctx.fill()
+    ctx.stroke()
+}
+
+var clearButton = document.getElementById("buttonClear")
+
+var wipeCanvas = function() {
+    ctx.clearRect(0, 0, c.clientWidth, c.height)
+}
+
+clearButton.addEventListener("click", wipeCanvas);
 
 c.addEventListener("click", draw)
-var bToggler = document.getElementById("buttonToggle")
-bToggler.addEventListener("click", toggleMode);
-var clearB = document.getElementById("buttonClear");
-clearB.addEventListener("click", wipeCanvas);
